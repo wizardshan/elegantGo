262a6b4c03e06a6b29b5a1aae7038656ed159118
iqiyi <iqiyi@11111deMac-mini.local>
2024-05-10 16:51:34 +0800

    xss

--- app/chapter3.0/main.go
+++ app/chapter3.0/main.go
@@ -3,25 +3,15 @@ package main
 import (
 	"app/chapter3.0/controller"
 	"app/chapter3.0/repository"
-	"database/sql"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 
-	dsn := "root:@tcp(127.0.0.1:3306)/test"
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		panic(err)
-	}
-
-	//handler := new(controller.Handler)
-
 	engine := gin.New()
 	engine.LoadHTMLGlob("controller/view/*.tmpl")
-	repoArticle := repository.NewArticle(db)
+	repoArticle := repository.NewArticle()
 	ctrArticle := controller.NewArticle(repoArticle)
 	engine.GET("/article/search", ctrArticle.Search)
 
